4e28c4b66b6db9c6901e236c106494af046b072e
HaolongEdward <edwardazazel@126.com>
2018-04-05 14:56:09 -0400

    project set up.

---
+++ attempt_1/Controllers/BuildingController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using attempt_1.Models;
+
+namespace attempt_1.Controllers
+{
+    public class BuildingController : Controller
+    {
+        // GET: Building
+        public ActionResult Random()
+        {
+            //    public int Building_Id { get; set; }
+            //public String Building_name { get; set; }
+            //public String City { get; set; }
+            //public String State { get; set; }
+            //public String Country { get; set; }
+            var building = new Building()
+            {
+                Building_Id = 1,
+                State = "Quebec",
+                City = "Montreal",
+                Building_name = "Centre Eaton - Montreal",
+                Country = "Canada"
+            }; 
+            return View(building);
+        }
+    }
+}
---
+++ attempt_1/Models/building.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace attempt_1.Models
+{
+    public class Building
+    {
+        //using the same layout from json
+
+        public int Building_Id { get; set; }
+        public String Building_name { get; set; }
+        public String City { get; set; }
+        public String State { get; set; }
+        public String Country { get; set; }
+        
+        
+    }
+}
